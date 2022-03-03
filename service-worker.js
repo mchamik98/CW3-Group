03a09b0f121794708a7e1def7674583ea28f9b59
mchamik98 <marcin.chamik@onet.pl>
2022-03-03 13:09:22 +0000

    d

--- service-worker.js
+++ service-worker.js
@@ -35,10 +35,3 @@ e.respondWith(
 });
 
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function (r){
-            return r
-        })
-    )
-});
